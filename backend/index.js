c4f5421fa79278b9c57a154b7dd6c30a76a505f8
Achal Gupta <achal02gupta@gmail.com>
2024-02-29 23:21:21 +0530

    [backend] - added cors for cross origin resource sharing

--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,16 @@ const port = 5000
 const mongoDB =require("./db.js")
 const cors=require('cors')
 mongoDB();
-
-app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 app.use(express.json())
 app.use(cors())
 app.use('/api',require("./Routes/CreateUser"))
 app.use('/api',require("./Routes/DisplayData"))
 app.use('/api',require("./Routes/OrderData"))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 
 app.listen(port, () => {
